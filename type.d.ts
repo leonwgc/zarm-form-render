51bf5b842474b69184eed069f0866946d7b20d9b
leonwgc <wangguochao@zuifuli.com>
2021-04-04 11:43:23 +0800

    update doc

--- type.d.ts
+++ type.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 export interface Item {
   type: React.Component; // 组件类型， 比如Input 等
   name: string; // Cell name
-  items?: Array<any>; // dataSource 
+  items?: Array<any>; // dataSource
   description: string; // Cell description
   label?: string; // Cell title
   render?: () => React.ReactNode; //自定义 render
@@ -11,9 +11,9 @@ export interface Item {
   cellProps?: object; // cell props配置
 }
 export interface Props {
-  layoutData: Array<Item>;
-  data: object;
-  setData: () => void;
+  layoutData: Array<Item>; // 表单布局配置，二维数组，一个子数组代码一个group块
+  data: object; // 数据存储,name作为key,内容为value
+  setData: () => void; // 数据更新
 }
 interface FormRenderer extends React.FC<Props> {}
 declare const FormRenderer: FormRenderer;
