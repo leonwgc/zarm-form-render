2fa873e66cad8073dfb1bfeb0392c6e31fcb8f73
leonwgc <giantfish@126.com>
2021-05-16 12:30:13 +0800

    chore: use vited as dev server

--- src/demo/App.jsx
+++ src/demo/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import FormRenderer from '../dist';
+import FormRenderer from '../index';
 import { Input, Cell, Radio, Select, DateSelect, Button, Toast, Panel } from 'zarm';
 import './App.less';
 
